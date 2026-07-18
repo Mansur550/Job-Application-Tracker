2df889cae177d50b1e3b5bca7f8e6b16e03b04df
Mansur550 <mansurislam59@gmail.com>
2026-07-18 18:26:29 +0600

    MongoDb mongoos

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
 
   return (
-    <div className="flex min-h-screen flex-col bg-white">Mansur
+    <div className="flex min-h-screen flex-col bg-white">
       <main className="flex-1">
         {/* Hero Section */}
         <section className="container mx-auto px-4 py-32">
