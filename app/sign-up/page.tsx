56698a370ee7486c0c4625b0f3c297a5c37770e2
Mansur550 <mansurislam59@gmail.com>
2026-07-20 18:23:30 +0600

    Card component from shadcn

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,6 +1,20 @@
+"use client";
 
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function SignUp() {
-    return <div>sign up
-    </div>
+    return(
+        <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Sign UP</CardTitle>
+
+                    <CardDescription> Create an account to start traking your JOb applications
+                    </CardDescription>
+                </CardHeader>
+                
+            </Card>
+
+        </div>
+    );
 }
