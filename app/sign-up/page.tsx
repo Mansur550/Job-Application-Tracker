13972958e8a72d16fba02fb9f35f1d3807943890
Mansur550 <mansurislam59@gmail.com>
2026-07-20 18:42:09 +0600

    SignUp page

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Link } from "lucide-react";
+
+
 
 export default function SignUp() {
-    return(
+    return (
         <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
             <Card>
                 <CardHeader>
@@ -15,16 +19,45 @@ export default function SignUp() {
                     </CardDescription>
                 </CardHeader>
                 <form action="">
-                   <CardContent>
-                    {/* Nme */}
-                    <div>
-                        <Label htmlFor="name">Name</Label>
-                        <Input id= "name" type="text" placeholder="Jhon Doe" required/>
-                    </div>
+                    <CardContent>
+                        {/* Nme */}
+                        <div>
+                            <Label htmlFor="name">Name</Label>
+                            <Input
+                                id="name"
+                                type="text"
+                                placeholder="Jhon Doe"
+                                required
+                            />
+                        </div>
 
-                    </CardContent> 
+                        {/* Email */}
+                        <div>
+                            <Label htmlFor="email">Email</Label>
+                            <Input id="email" type="email" placeholder="jhondoe@example.com" required />
+                        </div>
+
+                        {/* Password */}
+                        <div>
+                            <Label htmlFor="password">Password</Label>
+                            <Input
+                                id="password"
+                                type="password"
+                                placeholder=""
+                                required
+                            />
+                        </div>
+                    </CardContent>
+
+                    <CardFooter>
+                        <Button type="submit">Sign-up</Button>
+                        <p>
+                            Already have an account? <Link href="/sign-in">Sign In</Link>
+                        </p>
+                        
+                    </CardFooter>
                 </form>
-                
+
             </Card>
 
         </div>
