712e6ec834099ee03ede1774f8c631386a98e477
Mansur550 <mansurislam59@gmail.com>
2026-07-24 02:40:36 +0600

    AUth CLient

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -6,6 +6,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useState } from "react";
+import {signUp} from '@/lib/auth/auth-client'
 
 
 
