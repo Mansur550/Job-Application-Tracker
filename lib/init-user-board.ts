8c26f82f22bf4228465d3d7d41c944f592cd4c3b
Mansur550 <mansurislam59@gmail.com>
2026-07-25 13:12:00 +0600

    board columns

--- lib/init-user-board.ts
+++ lib/init-user-board.ts
@@ -2,13 +2,27 @@ import connectDB from "./db";
 import { Board, Column } from "./models";
 
 
+
+
 export async function initalizeUserBoard(userId: string){
     try{
         await connectDB()
 
         //Check if Board already exist?
-        
+         const existingBoard = await  Board.findOne({userId, name: "Job Hunt"})
 
+         if(existingBoard){
+            return existingBoard;
+         }
+         //Create the board
+         const board =await Board.create({
+            name: "Job Hunt",
+            userId,
+            columns:[
+
+            ],
+            //Create defult columns
+         })
 
     } catch (err){
 
