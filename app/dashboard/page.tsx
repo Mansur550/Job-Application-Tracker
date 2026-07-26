5eab1ed80a737955a1c5d80dac1c6b94d59f71bc
Mansur550 <mansurislam59@gmail.com>
2026-07-26 13:30:28 +0600

    proxy/ middlewire

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,14 @@
+import { getSession } from "@/lib/auth/auth"
+import { redirect } from "next/navigation"
 
 
-export default function Dashbord() {
+
+export default async function Dashbord() {
+    const session =await getSession()
+    if (!session?.user){
+        redirect("/sign-in")
+    }
+
     return <div>
         dashboard
     </div>
